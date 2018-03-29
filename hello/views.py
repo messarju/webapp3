0cd29412ecfbe879db3587d5ae9c4b386ff68e6f
Biojet1 <biojet1@gmail.com>
2018-03-30 02:08:24 +0800

    inb3

--- hello/views.py
+++ hello/views.py
@@ -70,7 +70,7 @@ def lave(request):
 
 @csrf_exempt
 def inb(request):
-	open("mail.txt", "w").write(repr(request.POST))
+	open("mail.txt", "w").write(repr(dir(request)))
 	return HttpResponse("OK")
 
 def db(request):
